42eb2e44894a34b1941e9f9acf526fdf454e01c4
jahangirali555 <jahangirali555@gmail.com>
2014-05-19 13:59:00 +0000

    recovery from a svere Error

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,23 @@
+class IndexController < ApplicationController
+  
+  def index
+    @title = "Index"
+  end
+  
+    
+  def shop
+    @title = "Index"
+  end
+
+  def events
+    @title = "Index"
+  end
+
+  def shop
+    @title = "Index"
+  end
+
+  def discussion
+    @title = "Index"
+  end
+end
